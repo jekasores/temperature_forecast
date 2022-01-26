e961dfc42028fc0a898e1126d52b2edf600937b1
jessqintess <92453874+jessqintess@users.noreply.github.com>
2022-01-26 09:38:07 -0300

    Moving function check_data_day() and check_data_month() from the notebook to the .py file

--- eda_functions.py
+++ eda_functions.py
@@ -1,3 +1,53 @@
+#Libraries
+import matplotlib.pyplot as plt
+
+#Functions
+
+def check_data_day(df, year, month):
+    ''' This function prints the statistics about number of registers available per day
+    given a month and a year
+
+    INPUT: 
+            df (dataframe)
+            year (string)
+            month (string)
+    OUTPUT: None
+    '''
+    
+    #Print total number of registers in a month
+    num_registers = df[(df.year==year) & (df.month==month)].month.value_counts().to_list()[0]
+    print("There are {} registers for this month".format(num_registers))
+    
+    #Print the total number of days considered in that month
+    num_days = len(set(df[(df.year==year) & (df.month==month)].day.value_counts().keys().sort_values().to_list()))
+    print("There are {} days registered".format(num_days))
+    
+    #Print basic statistics about the register in the month
+    measure_per_day = df[(df.year==year) & (df.month==month)].day.value_counts().to_list()
+    print("The minimum amount of registers in a day for this month was {}".format(min(measure_per_day)))
+    print("The maximum amount of registers in a day for this month was {}".format(max(measure_per_day)))
+    print("The mean amount of registers for this month was {}".format(sum(measure_per_day)/len(measure_per_day)))
+    
+    #Show number of register per day as a bar plot
+    df[(df.year==year) & (df.month==month)].day.value_counts().sort_index().plot(kind = 'bar')
+    plt.title("Number of registers per day from month {} of {}".format(month, year));
+    plt.xlabel('Day')
+    plt.ylabel('Number of registers')
+    plt.pause(0.05) #Add to show plot during for loop
+
+def check_data_month(df, year):
+    ''' This function prints the number of registers available per year and also per month
+    INPUT: 
+            df (dataframe)
+            year (string)
+    OUTPUT: None
+    '''
+    df = df[df.year==year]
+    print("Checking if there is only data from the selected year: ")
+    print(df.year.value_counts())
+    print("Checking the months available: ")
+    print(df.month.value_counts())
+
 def df_dimensions(df):
     '''
     INPUT: Dataframe
